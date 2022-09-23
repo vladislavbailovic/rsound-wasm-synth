d1a2372fd8523bbd3af59636c54a2db00323e63e
vladislav <ve@incsub.com>
2022-09-23 19:34:32 +0200

    Start preparing the synth and modulator data

--- ts/site.tsx
+++ ts/site.tsx
@@ -1,18 +1,34 @@
-const Interface = () => (<>
-	<Display id="graph" />
-	<Synth type="chain" />
+import init, { play, draw, draw_lfo, draw_oscillator } from "../pkg/rsound_wasm_synth.js";
+
+class ModulatorData {
+	kind: number = 0
+	shape: number = 0
+	freq: number = 0
+};
+
+class SynthData {
+	tone: number = 0
+	modulators: Array<ModulatorData> = []
+}
+
+const Interface = ({synth}: {synth: SynthData}) => (<>
+	<Synth type="chain" synth={ synth } />
 	<Keyboard />
 </>);
 
-const Display = ({ id }: { id?: string | undefined}) => {
+const Display = ({ id, src }: { id?: string, src?: string }) => {
 	return <div className="display">
-		<img id={id} className="graph" src="" />
+		<img id={id} className="graph" src={src} />
 	</div>;
 };
 
-const Synth = ({ type }: { type: string }) => {
+const Synth = ({ type, synth }: { type: string, synth: SynthData }) => {
 	const cls = ["synth"].concat([type]).join(" ");
+	const graph = draw(synth.tone, 0, synth.modulators);
+	const blob = new Blob([graph], {type: "image/svg+xml"});
+	const temp_url = window.URL.createObjectURL(blob);
 	return <div className={cls}>
+		<Display id="graph" src={temp_url} />
 		<SynthSource />
 		<Modulators />
 	</div>;
@@ -90,8 +106,16 @@ const Link = ({ type, children }: { type: string, children: Array<JSX.Element> }
 	</div>
 };
 
-// @ts-expect-error
-ReactDOM.render(
-	<Interface />,
-	document.getElementById('interface')
-);
+init().then(res => {
+	const synth = {
+		tone: 0,
+		modulators: [
+			{ kind: 0, shape: 0, freq: 45 },
+		],
+	};
+	// @ts-expect-error
+	ReactDOM.render(
+		<Interface synth={synth} />,
+		document.getElementById('interface')
+	);
+});
