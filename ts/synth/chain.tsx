0c13b6e7c733f3d9a3c94a204e356ad0d0370964
vladislav <ve@incsub.com>
2022-10-02 05:02:00 +0200

    Insert modulators and setup defaults

--- ts/synth/chain.tsx
+++ ts/synth/chain.tsx
@@ -62,37 +62,41 @@ const Modulator = ({
   const blob = new Blob([graph], { type: 'image/svg+xml' });
   const tempUrl = window.URL.createObjectURL(blob);
 
-	const synthCtx = useContext(SynthDataContext);
-	const del = (): void => {
-		const modulators = [...synthCtx.data.modulators];
-		modulators.splice(idx, 1);
-		synthCtx.setData({...synthCtx.data, modulators });
-	};
-	const changeFreq = (hz: number): void => {
-		const modulators = [...synthCtx.data.modulators];
-		modulators[idx].freq = hz;
-		synthCtx.setData({...synthCtx.data, modulators });
-	};
-	const changeShape = (shape: number): void => {
-		const modulators = [...synthCtx.data.modulators];
-		modulators[idx].shape = shape;
-		synthCtx.setData({...synthCtx.data, modulators });
-	};
+  const synthCtx = useContext(SynthDataContext);
+  const del = (): void => {
+    const modulators = [...synthCtx.data.modulators];
+    modulators.splice(idx, 1);
+    synthCtx.setData({ ...synthCtx.data, modulators });
+  };
+  const changeFreq = (hz: number): void => {
+    const modulators = [...synthCtx.data.modulators];
+    modulators[idx].freq = hz;
+    synthCtx.setData({ ...synthCtx.data, modulators });
+  };
+  const changeShape = (shape: number): void => {
+    const modulators = [...synthCtx.data.modulators];
+    modulators[idx].shape = shape;
+    synthCtx.setData({ ...synthCtx.data, modulators });
+  };
 
   return (
-    <Link type="modulator" graph={tempUrl} del={del}>
+    <Link type="modulator" graph={tempUrl} del={del} idx={idx}>
       <input type="hidden" value="1" />
       <label>
         <span className="kind"></span>
-        <select onChange={ e => changeShape(Number(e.target.selectedIndex)) }>
-          <option>Sine</option>
-          <option>Square</option>
-          <option>Triangle</option>
-          <option>Saw</option>
+        <select onChange={(e) => changeShape(Number(e.target.value))} value={modulator.shape} >
+          <option value="0">Sine</option>
+          <option value="1">Square</option>
+          <option value="2">Triangle</option>
+          <option value="3">Saw</option>
         </select>
       </label>
       <label>
-        <input type="numeric" defaultValue="45" onChange={ e => changeFreq(Number(e.target.value)) } />
+        <input
+          type="numeric"
+          value={modulator.freq}
+          onChange={(e) => changeFreq(Number(e.target.value))}
+        />
         <span>Hz</span>
       </label>
     </Link>
@@ -102,29 +106,34 @@ const Modulator = ({
 const Link = ({
   type,
   graph,
+  idx,
   del,
   children
 }: {
   type: string
+  idx?: number | null
   graph?: string | undefined
   del?: null | (() => void)
   children: JSX.Element[]
 }): JSX.Element => {
   const cls = ['link'].concat([type]).join(' ');
-	const synthCtx = useContext(SynthDataContext);
+  const synthCtx = useContext(SynthDataContext);
+	let nextPosition = idx || 0;
 
-	const add = (): void => {
-		const modulators = [...synthCtx.data.modulators];
-		modulators.push(new ModulatorData());
-		synthCtx.setData({...synthCtx.data, modulators });
-	};
+  const add = (): void => {
+    const modulators = [...synthCtx.data.modulators];
+    modulators.splice(nextPosition, 0, new ModulatorData());
+    synthCtx.setData({ ...synthCtx.data, modulators });
+  };
 
   let kill = null;
   if (type === 'modulator') {
-	const delHandler = del
-		? (e: React.UIEvent):void => del()
-		: undefined;
-    kill = <button onClick={delHandler} className="kill">[x]</button>;
+    const delHandler = (del != null) ? (e: React.UIEvent): void => del() : undefined;
+    kill = (
+      <button onClick={delHandler} className="kill">
+        [x]
+      </button>
+    );
   }
 
   return (
@@ -135,8 +144,12 @@ const Link = ({
       <div className="params">{children}</div>
       <div className="next">
         {kill}
-        <button className="add" onClick={e => add()}>Add</button>
-        <button className="sub" onClick={e => add()}>Sub</button>
+        <button className="add" onClick={(e) => add()}>
+          Add
+        </button>
+        <button className="sub" onClick={(e) => add()}>
+          Sub
+        </button>
       </div>
     </div>
   );
