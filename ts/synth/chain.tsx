c6d3dd2cef804f4f93747bd6b703c53e948fd373
vladislav <ve@incsub.com>
2022-10-05 05:51:47 +0200

    Start refactoring modulators UI

--- ts/synth/chain.tsx
+++ ts/synth/chain.tsx
@@ -1,20 +1,9 @@
 import React, { useContext } from 'react';
 import { Display } from '../display';
-import { SynthDataContext, ModulatorData } from '../data';
-import {
-  draw,
-  draw_lfo,
-  Oscillator,
-  EnvelopeFactory,
-  ModulatorKind,
-  ModulatorOp
-} from '../../pkg/rsound_wasm_synth';
+import { SynthDataContext } from '../data';
+import { Modulators, Next } from './modulator';
+import { draw } from '../../pkg/rsound_wasm_synth';
 import './chain.css';
-
-enum LinkType {
-  Source = 1,
-  Modulator,
-}
 
 export const Synth = ({ type }: { type: string }): JSX.Element => {
   const synthCtx = useContext(SynthDataContext);
@@ -37,193 +26,20 @@ export const Synth = ({ type }: { type: string }): JSX.Element => {
 
 const SynthSource = (): JSX.Element => {
   return (
-    <Link type={LinkType.Source}>
-      <label>
-        <select>
-          <option>Sine</option>
-          <option>Square</option>
-        </select>
-      </label>
-      <label>
-        <input type="number" defaultValue="440" min="20" max="20000" />
-        <span>Hz</span>
-      </label>
-    </Link>
-  );
-};
-
-const Modulators = ({
-  modulators
-}: {
-  modulators: ModulatorData[]
-}): JSX.Element => (
-  <>
-    {modulators.map((mod, idx) => (
-      <Modulator key={idx} modulator={mod} idx={idx} />
-    ))}
-  </>
-);
-
-const Modulator = ({
-  modulator,
-  idx
-}: {
-  modulator: ModulatorData
-  idx: number
-}): JSX.Element => {
-  const graph = draw_lfo(modulator);
-  const blob = new Blob([graph], { type: 'image/svg+xml' });
-  const tempUrl = window.URL.createObjectURL(blob);
-
-  const operation = ModulatorOp[modulator.op];
-  const kind = ModulatorKind[modulator.kind];
-
-  const synthCtx = useContext(SynthDataContext);
-  const del = (): void => {
-    const modulators = [...synthCtx.data.modulators];
-    modulators.splice(idx, 1);
-    synthCtx.setData({ ...synthCtx.data, modulators });
-  };
-  const changeFreq = (hz: number): void => {
-    const modulators = [...synthCtx.data.modulators];
-    modulators[idx].freq = hz;
-    synthCtx.setData({ ...synthCtx.data, modulators });
-  };
-  const changeShape = (shape: Oscillator): void => {
-    const modulators = [...synthCtx.data.modulators];
-    modulators[idx].shape = shape;
-    console.log(shape, modulators);
-    synthCtx.setData({ ...synthCtx.data, modulators });
-  };
-
-  return (
-    <Link type={LinkType.Modulator} graph={tempUrl} del={del} idx={idx}>
-      <fieldset>
-        <title>
-          {operation} {kind}
-        </title>
-
-        {Object.entries(Oscillator)
-          .filter(([key, val]) => !isNaN(Number(val)))
-          .map(([key, val]) => {
-            const rkey = `${key}-${Number(val)}`;
-            const name = `shape-${idx}`;
-            return (
-              <label key={rkey}>
-                <input
-                  type="radio"
-                  name={name}
-                  onChange={(e) => changeShape(Number(val))}
-                  value={Number(val)}
-                  checked={Number(val) === modulator.shape}
-                />
-                <span>{key}</span>
-              </label>
-            );
-          })}
-
+    <div className="link source">
+      <div className="params">
         <label>
-          <input
-            type="number"
-            value={modulator.freq}
-            onChange={(e) => changeFreq(Number(e.target.value))}
-            min="20"
-            max="20000"
-          />
+          <select>
+            <option>Sine</option>
+            <option>Square</option>
+          </select>
+        </label>
+        <label>
+          <input type="number" defaultValue="440" min="20" max="20000" />
           <span>Hz</span>
         </label>
-      </fieldset>
-    </Link>
-  );
-};
-
-const Link = ({
-  type,
-  graph,
-  idx,
-  del,
-  children
-}: {
-  type: LinkType
-  idx?: number | null
-  graph?: string | undefined
-  del?: null | (() => void)
-  children: JSX.Element | JSX.Element[]
-}): JSX.Element => {
-  const typeClass = type === LinkType.Source ? 'source' : 'modulator';
-  const cls = ['link'].concat([typeClass]).join(' ');
-  const synthCtx = useContext(SynthDataContext);
-  const nextPosition = idx == null ? 0 : idx;
-
-  const injectModulator = (mod: ModulatorData): void => {
-    const modulators = [...synthCtx.data.modulators];
-    modulators.splice(nextPosition + 1, 0, mod);
-    synthCtx.setData({ ...synthCtx.data, modulators });
-  };
-  const addLfo = (): void =>
-    injectModulator(
-      ModulatorData.from({ op: ModulatorOp.Add, kind: ModulatorKind.LFO })
-    );
-  const subLfo = (): void =>
-    injectModulator(
-      ModulatorData.from({ op: ModulatorOp.Sub, kind: ModulatorKind.LFO })
-    );
-  const addElfo = (): void =>
-    injectModulator(
-      ModulatorData.from({
-        op: ModulatorOp.Add,
-        kind: ModulatorKind.ELFO,
-        env: EnvelopeFactory.ASR(13, 12, 161)
-      })
-    );
-  const subElfo = (): void =>
-    injectModulator(
-      ModulatorData.from({
-        op: ModulatorOp.Add,
-        kind: ModulatorKind.ELFO,
-        env: EnvelopeFactory.ASR(13, 12, 161)
-      })
-    );
-
-  let kill = null;
-  if (type === LinkType.Modulator) {
-    const delHandler =
-      del != null ? (e: React.UIEvent): void => del() : undefined;
-    kill = (
-      <button onClick={delHandler} className="kill">
-        [x]
-      </button>
-    );
-  }
-
-  let shape = null;
-  if (type === LinkType.Modulator) {
-    shape = (
-      <div className="shape">
-        <Display src={graph} />
       </div>
-    );
-  }
-
-  return (
-    <div className={cls}>
-      {shape}
-      <div className="params">{children}</div>
-      <div className="next">
-        {kill}
-        <button className="add" onClick={(e) => addLfo()}>
-          Add LFO
-        </button>
-        <button className="add" onClick={(e) => addElfo()}>
-          Add ELFO
-        </button>
-        <button className="sub" onClick={(e) => subLfo()}>
-          Sub LFO
-        </button>
-        <button className="sub" onClick={(e) => subElfo()}>
-          Sub ELFO
-        </button>
-      </div>
+      <Next />
     </div>
   );
 };
---
+++ ts/synth/modulator.tsx
@@ -0,0 +1,170 @@
+import React, { useContext } from 'react';
+import { Display } from '../display';
+import { SynthDataContext, ModulatorData } from '../data';
+import {
+  draw_lfo,
+  Oscillator,
+  EnvelopeFactory,
+  ModulatorKind,
+  ModulatorOp
+} from '../../pkg/rsound_wasm_synth';
+import './chain.css';
+
+export const Modulators = ({
+  modulators
+}: {
+  modulators: ModulatorData[]
+}): JSX.Element => (
+  <>
+    {modulators.map((mod, idx) => (
+      <Modulator key={idx} modulator={mod} idx={idx} />
+    ))}
+  </>
+);
+
+const Modulator = ({
+  modulator,
+  idx
+}: {
+  modulator: ModulatorData
+  idx: number
+}): JSX.Element => {
+  const graph = draw_lfo(modulator);
+  const blob = new Blob([graph], { type: 'image/svg+xml' });
+  const tempUrl = window.URL.createObjectURL(blob);
+
+  const operation = ModulatorOp[modulator.op];
+  const kind = ModulatorKind[modulator.kind];
+
+  const synthCtx = useContext(SynthDataContext);
+  const del = (): void => {
+    const modulators = [...synthCtx.data.modulators];
+    modulators.splice(idx, 1);
+    synthCtx.setData({ ...synthCtx.data, modulators });
+  };
+  const changeFreq = (hz: number): void => {
+    const modulators = [...synthCtx.data.modulators];
+    modulators[idx].freq = hz;
+    synthCtx.setData({ ...synthCtx.data, modulators });
+  };
+  const changeShape = (shape: Oscillator): void => {
+    const modulators = [...synthCtx.data.modulators];
+    modulators[idx].shape = shape;
+    console.log(shape, modulators);
+    synthCtx.setData({ ...synthCtx.data, modulators });
+  };
+
+  return (
+    <div className="link modulator">
+      <div className="shape">
+        <Display src={tempUrl} />
+      </div>
+      <div className="params">
+        <fieldset>
+          <title>
+            {operation} {kind}
+          </title>
+
+          {Object.entries(Oscillator)
+            .filter(([key, val]) => !isNaN(Number(val)))
+            .map(([key, val]) => {
+              const rkey = `${key}-${Number(val)}`;
+              const name = `shape-${idx}`;
+              return (
+                <label key={rkey}>
+                  <input
+                    type="radio"
+                    name={name}
+                    onChange={(e) => changeShape(Number(val))}
+                    value={Number(val)}
+                    checked={Number(val) === modulator.shape}
+                  />
+                  <span>{key}</span>
+                </label>
+              );
+            })}
+
+          <label>
+            <input
+              type="number"
+              value={modulator.freq}
+              onChange={(e) => changeFreq(Number(e.target.value))}
+              min="20"
+              max="20000"
+            />
+            <span>Hz</span>
+          </label>
+        </fieldset>
+      </div>
+      <Next idx={idx} del={del} />
+    </div>
+  );
+};
+
+export const Next = ({
+  idx,
+  del
+}: {
+  idx?: number | null
+  del?: null | (() => void)
+}): JSX.Element => {
+  const synthCtx = useContext(SynthDataContext);
+  const nextPosition = idx == null ? 0 : idx;
+
+  const injectModulator = (mod: ModulatorData): void => {
+    const modulators = [...synthCtx.data.modulators];
+    modulators.splice(nextPosition + 1, 0, mod);
+    synthCtx.setData({ ...synthCtx.data, modulators });
+  };
+  const addLfo = (): void =>
+    injectModulator(
+      ModulatorData.from({ op: ModulatorOp.Add, kind: ModulatorKind.LFO })
+    );
+  const subLfo = (): void =>
+    injectModulator(
+      ModulatorData.from({ op: ModulatorOp.Sub, kind: ModulatorKind.LFO })
+    );
+  const addElfo = (): void =>
+    injectModulator(
+      ModulatorData.from({
+        op: ModulatorOp.Add,
+        kind: ModulatorKind.ELFO,
+        env: EnvelopeFactory.ASR(13, 12, 161)
+      })
+    );
+  const subElfo = (): void =>
+    injectModulator(
+      ModulatorData.from({
+        op: ModulatorOp.Add,
+        kind: ModulatorKind.ELFO,
+        env: EnvelopeFactory.ASR(13, 12, 161)
+      })
+    );
+
+  let kill = null;
+  if (del != null) {
+    kill = (
+      <button onClick={(e: React.UIEvent): void => del()} className="kill">
+        [x]
+      </button>
+    );
+  }
+
+  return (
+    <div className="next">
+      {kill}
+      <button className="add" onClick={(e) => addLfo()}>
+        Add LFO
+      </button>
+      <button className="add" onClick={(e) => addElfo()}>
+        Add ELFO
+      </button>
+      <button className="sub" onClick={(e) => subLfo()}>
+        Sub LFO
+      </button>
+      <button className="sub" onClick={(e) => subElfo()}>
+        Sub ELFO
+      </button>
+    </div>
+  );
+};
