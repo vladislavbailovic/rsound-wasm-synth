50957620591496446914de7f19132d1578269386
vladislav <ve@incsub.com>
2022-10-09 04:01:23 +0200

    Clean up dead code

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@ extern "C" {
 }
 
 use instrument::generator::Signal;
-use instrument::oscillator::*;
 use instrument::*;
 use note::*;
 use rsound_output::Buffer; // TODO: refactor PcmRenderer into f32 buffer getter for web audio and
@@ -110,41 +109,6 @@ pub fn draw_env(raw: JsValue) -> Vec<u8> {
         result[i] = env.value_at(*t, 1.0);
     }
     graph(&result)
-}
-
-pub fn get_synth_sound(tone: i32, base: i32, mods: Vec<JsValue>) -> Vec<f64> {
-    let pc = match tone {
-        0 => PitchClass::C,
-        1 => PitchClass::Cis,
-        2 => PitchClass::D,
-        3 => PitchClass::Dis,
-        4 => PitchClass::E,
-        5 => PitchClass::F,
-        6 => PitchClass::Fis,
-        7 => PitchClass::G,
-        8 => PitchClass::Gis,
-        9 => PitchClass::A,
-        10 => PitchClass::B,
-        11 => PitchClass::H,
-        _ => todo!(),
-    };
-    let n = Note::Tone(pc, Octave::C3, val![1 / 4]);
-
-    let envelope = envelope::ASR::new(0.015, 0.7, 0.2);
-    let mut chain = match base {
-        1 => generator::chain::Chain::new(Oscillator::Square),
-        _ => generator::chain::Chain::new(Oscillator::Sine),
-    };
-    for res in mods {
-        let modulator: ModulatorRawData = serde_wasm_bindgen::from_value(res).unwrap();
-        match modulator.op.into() {
-            ModulatorOp::Add => chain.add_box(modulator.into()),
-            ModulatorOp::Sub => chain.sub_box(modulator.into()),
-        };
-    }
-
-    let synth = Instrument::new(chain, envelope);
-    synth.play(90.0, n, 1.0)
 }
 
 use graph::svg::Renderer;
